a97daedc02d35040fc0a2755557b236f260d98c9
Abner Andino <brain.agam@gmail.com>
2022-07-08 16:49:30 +0100

    lua configuration

--- lua/bighelmet7/init.lua
+++ lua/bighelmet7/init.lua
@@ -1,6 +1,37 @@
 -- TODO (bighelmet7): migrate init.vim to lua format
 -- TODO (bighelmet7): it would be nice if we could move this into separate files
 
+-- vim set
+vim.g.mapleader = " "
+
+-- global
+vim.go.hlsearch = false
+vim.go.hidden = true
+vim.go.errorbells = false
+vim.go.backup = false
+vim.go.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.go.incsearch = true
+vim.go.scrolloff = 10
+vim.go.cmdheight = 2
+
+-- local to window
+vim.wo.number = true
+vim.wo.wrap = false
+vim.wo.colorcolumn = "80"
+vim.wo.signcolumn = "yes"
+
+-- local to buffer
+vim.bo.swapfile = false
+vim.bo.undofile = true
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
+vim.bo.smartindent = true
+
+
+-- Plugins
+--
 -- nvim-tree.lua and nvim-web-devicons
 require("nvim-web-devicons").setup{ default = true }
 require("nvim-tree").setup({
@@ -21,4 +52,26 @@ vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require"telescope.builtin".grep
 
 -- LazyGit
 vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<cr>', { noremap = true, silent = true })
+
+-- CoC
+--
+vim.g.coc_global_extensions = {
+    "coc-snippets",
+    "coc-pyright",
+    "coc-go",
+    "coc-spell-checker",
+    "coc-rls",
+    "coc-json",
+    "coc-html-css-support",
+    "coc-prettier",
+}
+
+-- coc-go
+vim.api.nvim_command("autocmd BufWritePre *.go :silent call CocAction('runCommand', 'editor.action.organizeImport')")
+
+-- Highlights
+--
+vim.cmd("colorscheme gruvbox")
+vim.cmd("highlight ColorColumn ctermbg=lightgrey guibg=lightgrey")
+vim.cmd("highlight Normal guibg=none")
 
