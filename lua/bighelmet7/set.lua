cda6b37dca38295fe0a67f94f51c4360003ddc53
Abner Andino <brain.agam@gmail.com>
2022-07-24 22:04:42 +0100

    vim options

---
+++ lua/bighelmet7/set.lua
@@ -0,0 +1,28 @@
+-- mapleader
+vim.g.mapleader = " "
+
+-- global options
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.incsearch = true
+vim.opt.scrolloff = 10
+vim.opt.cmdheight = 1
+
+-- local to window
+vim.opt.nu = true
+vim.opt.wrap = false
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+
+-- local to buffer
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
