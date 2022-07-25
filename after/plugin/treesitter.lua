bb9f97d5089d26838d4765540044da328485d914
Abner Andino <brain.agam@gmail.com>
2022-07-25 08:50:40 +0100

    add list of mandatory parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -25,6 +25,8 @@ require'nvim-treesitter.configs'.setup {
     },
     sync_install = false,
 
+    ignore_install = { "php", "phpdoc" },
+
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
