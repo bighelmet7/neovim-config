db6ed32b471bc3958af30037ef98a6705e487032
Abner Andino <brain.agam@gmail.com>
2022-07-25 02:19:25 +0100

    add treesitter

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,33 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+        "c",
+        "lua",
+        "yaml",
+        "html",
+        "css",
+        "bash",
+        "dockerfile",
+        "dot",
+        "python",
+        "go",
+        "rust",
+        "graphql",
+        "json",
+        "make",
+        "markdown",
+        "regex",
+        "svelte",
+        "tsx",
+        "toml",
+        "typescript",
+        "javascript",
+        "sql"
+    },
+    sync_install = false,
+
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
