6397c54223a863ae136942fc7f07afaf975acd44
Abner Andino <brain.agam@gmail.com>
2022-08-30 15:24:24 +0100

    use dap-python instead of manual configuration

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -6,33 +6,13 @@ dap.set_log_level("DEBUG")
 
 local cwd = vim.fn.getcwd()
 local poetry_bin = cwd .. "/../.env/bin/poetry"
-local homebrew_bin = "/opt/homebrew/bin"
+local env_path = helper.execute_cmd(table.concat({poetry_bin, "env", "info", "-p"}, " "), true)
+local python_env = env_path .. "/bin/python"
 
 -- Python
-dap.adapters.python = {
-    type = "executable",
-    command = poetry_bin,
-    args = {"run", "python", "-m", "debugpy.adapter"},
-    options = {cwd = cwd}
-}
-dap.configurations.python = {
-    {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        module = "pytest",
-        pythonPath = function()
-            local env_cmd = table.concat({poetry_bin, "env", "info", "-p"}, " ")
-            local env_path = helper.execute_cmd(env_cmd, true)
-            local python_env = env_path .. "/bin/python"
-            if vim.fn.executable(python_env) == 1 then
-                return python_env
-            else
-                return homebrew_bin .. "/python3"
-            end
-        end
-    }
-}
+local dap_python = require("dap-python")
+dap_python.test_runner = "pytest"
+dap_python.setup(python_env)
 
 -- Dap UI
 -- More: https://github.com/rcarriga/nvim-dap-ui
