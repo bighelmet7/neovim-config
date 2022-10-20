3ff583c0435585998e488910f5688b983ca27323
Abner Andino <brain.agam@gmail.com>
2022-10-20 10:08:02 +0100

    update cmp_nvim_lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -46,7 +46,7 @@ local function config(_config)
         "force",
         {
             -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-            capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+            capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
             on_attach = function()
                 nnoremap(
                     "gd",
