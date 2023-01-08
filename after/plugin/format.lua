bbab7ee31e5279e7366574fd4f266a0882111aef
Abner Andino <brain.agam@gmail.com>
2023-01-08 17:13:59 +0000

    rollback lspzero due performance impact

--- after/plugin/format.lua
+++ after/plugin/format.lua
@@ -1,6 +1,17 @@
+local nnoremap = require("bighelmet7.keymap").nnoremap
+
+-- run all formatters until one runs successfully
+vim.g.neoformat_run_all_formatters = 1
+
+-- nnoremap("<leader>nf", ":Neoformat<CR>")
+
 -- Format after :w
-local fmt = vim.api.nvim_create_augroup("fmt", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-        command = "LspZeroFormat",
-    group = fmt,
-})
+local fmt = vim.api.nvim_create_augroup("fmt", {clear = true})
+vim.api.nvim_create_autocmd(
+    "BufWritePre",
+    -- NOTE: https://github.com/sbdchd/neoformat/issues/134#issuecomment-1034726786
+    {
+        command = "try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry",
+        group = fmt
+    }
+)
--- lua/bighelmet7/packer.lua
+++ lua/bighelmet7/packer.lua
@@ -6,28 +6,16 @@ return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
 
     -- lsp
-    --
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    })
+    -- NOTE: nvim-lspconfig doesn't configure keymap by default:
+    -- https://github.com/neovim/nvim-lspconfig#suggested-configuration
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("L3MON4D3/LuaSnip") -- Snippets plugin
+    use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
 
     -- dap
     --
@@ -38,7 +26,7 @@ return require("packer").startup(function(use)
 
     -- formatter
     --
-    use("mhartington/formatter.nvim")
+    use("sbdchd/neoformat")
 
     -- theme
     --
@@ -103,7 +91,7 @@ return require("packer").startup(function(use)
     --
     use({
         "iamcco/markdown-preview.nvim",
-        run = function()vim.fn["mkdp#util#install"]() end,
+        run = function() vim.fn["mkdp#util#install"]() end,
     })
 
     -- tmux
