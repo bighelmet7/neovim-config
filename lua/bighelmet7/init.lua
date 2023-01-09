12f8125a116e5c8c002fb99cdd2134d538746b85
Abner Andino <brain.agam@gmail.com>
2023-01-09 21:40:18 +0000

    formatting

--- lua/bighelmet7/init.lua
+++ lua/bighelmet7/init.lua
@@ -10,48 +10,42 @@ vim.opt.termguicolors = true
 -- Plugins
 --
 -- nvim-tree.lua and nvim-web-devicons
-require("nvim-web-devicons").setup({default = true})
-require("nvim-tree").setup(
-    {
-        git = {ignore = false},
-        view = {side = "left"},
-        renderer = {
-            icons = {
-                glyphs = {
-                    git = {
-                        untracked = "🤷",
-                        unstaged = "👀"
-                    }
-                }
-            }
-        }
-    }
-)
+require("nvim-web-devicons").setup({ default = true })
+require("nvim-tree").setup({
+	git = { ignore = false },
+	view = { side = "left" },
+	renderer = {
+		icons = {
+			glyphs = {
+				git = {
+					untracked = "🤷",
+					unstaged = "👀",
+				},
+			},
+		},
+	},
+})
 
 -- Telescope
 require("telescope.builtin")
 
 -- Lualine
-require("lualine").setup(
-    {
-        options = {
-            theme = "rose-pine"
-        }
-    }
-)
+require("lualine").setup({
+	options = {
+		theme = "rose-pine",
+	},
+})
 
 -- bufferline
 --
-require("bufferline").setup(
-    {
-        options = {
-            offsets = {
-                {filetype = "NvimTree", text = "File Explorer", padding = 1}
-            },
-            max_name_length = 14,
-            max_prefix_length = 13,
-            tab_size = 20,
-            separator_style = "thin"
-        }
-    }
-)
+require("bufferline").setup({
+	options = {
+		offsets = {
+			{ filetype = "NvimTree", text = "File Explorer", padding = 1 },
+		},
+		max_name_length = 14,
+		max_prefix_length = 13,
+		tab_size = 20,
+		separator_style = "thin",
+	},
+})
