88ad8a2422ca0fee6a57e9e1a39a0cf1f389125d
Abner Andino <brain.agam@gmail.com>
2023-01-15 01:46:37 +0000

    update packer

--- lua/bighelmet7/packer.lua
+++ lua/bighelmet7/packer.lua
@@ -1,134 +1,116 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
-return require("packer").startup(
-    function(use)
-        -- Packer can manage itself
-        use("wbthomason/packer.nvim")
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-        -- lsp
-        -- NOTE: nvim-lspconfig doesn't configure keymap by default:
-        -- https://github.com/neovim/nvim-lspconfig#suggested-configuration
-        use("neovim/nvim-lspconfig")
-        use("hrsh7th/nvim-cmp")
-        use("hrsh7th/cmp-nvim-lsp")
-        use("hrsh7th/cmp-buffer")
-        use("hrsh7th/cmp-path")
-        use("hrsh7th/cmp-cmdline")
-        use("L3MON4D3/LuaSnip") -- Snippets plugin
-        use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	-- lsp
+	-- NOTE: nvim-lspconfig doesn't configure keymap by default:
+	-- https://github.com/neovim/nvim-lspconfig#suggested-configuration
+	use("neovim/nvim-lspconfig")
+	use({ "hrsh7th/nvim-cmp", requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" } })
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
 
-        -- dap
-        --
-        use(
-            {
-                "mfussenegger/nvim-dap",
-                ensure_dependencies = true,
-                requires = {
-                    "rcarriga/nvim-dap-ui",
-                    "leoluz/nvim-dap-go",
-                    "mfussenegger/nvim-dap-python"
-                }
-            }
-        )
+	-- dap
+	--
+	use({
+		"mfussenegger/nvim-dap",
+		ensure_dependencies = true,
+		requires = {
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+			"mfussenegger/nvim-dap-python",
+		},
+	})
 
-        -- formatter
-        --
-        use("sbdchd/neoformat")
+	-- formatter
+	--
+	use("sbdchd/neoformat")
+	use("lukas-reineke/indent-blankline.nvim")
 
-        -- theme
-        --
-        use(
-            {
-                "rose-pine/neovim",
-                as = "rose-pine"
-            }
-        )
+	-- theme
+	--
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+	})
 
-        -- nvim-tree
-        --
-        use(
-            {
-                "kyazdani42/nvim-tree.lua",
-                requires = {
-                    "kyazdani42/nvim-web-devicons" -- optional, for file icons
-                },
-                tag = "nightly" -- optional, updated every week. (see issue #1193)
-            }
-        )
+	-- nvim-tree
+	--
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icons
+		},
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
 
-        -- telescope
-        --
-        use(
-            {
-                "nvim-telescope/telescope.nvim",
-                tag = "0.1.0",
-                requires = {{"nvim-lua/plenary.nvim"}}
-            }
-        )
+	-- telescope
+	--
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-        -- ripgrep
-        --
-        use("BurntSushi/ripgrep")
+	-- ripgrep
+	--
+	use("BurntSushi/ripgrep")
 
-        -- lazygit
-        --
-        use("kdheepak/lazygit.nvim")
-        use("f-person/git-blame.nvim")
+	-- lazygit
+	--
+	use("kdheepak/lazygit.nvim")
+	use("f-person/git-blame.nvim")
 
-        -- treesitter
-        -- treesitter vs LSP: https://github.com/nvim-treesitter/nvim-treesitter/issues/484#issuecomment-694388223
-        --
-        use(
-            {
-                -- it will fail the first time
-                -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
-                "nvim-treesitter/nvim-treesitter",
-                run = ":TSUpdate"
-            }
-        )
+	-- treesitter
+	-- treesitter vs LSP: https://github.com/nvim-treesitter/nvim-treesitter/issues/484#issuecomment-694388223
+	--
+	use({
+		-- it will fail the first time
+		-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+	})
 
-        -- statusline
-        --
-        use(
-            {
-                "nvim-lualine/lualine.nvim",
-                requires = {"kyazdani42/nvim-web-devicons", opt = true}
-            }
-        )
+	-- statusline
+	--
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-        -- bufferline (mainly for tabs and groups)
-        --
-        use({"akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons"})
+	-- bufferline (mainly for tabs and groups)
+	--
+	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
 
-        -- sqls
-        --
-        use("nanotee/sqls.nvim")
+	-- sqls
+	--
+	use("nanotee/sqls.nvim")
 
-        -- marksdown preview
-        --
-        use(
-            {
-                "iamcco/markdown-preview.nvim",
-                run = function()
-                    vim.fn["mkdp#util#install"]()
-                end
-            }
-        )
+	-- marksdown preview
+	--
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-        -- tmux
-        --
-        use(
-            {
-                "aserowy/tmux.nvim",
-                config = function()
-                    require("tmux").setup()
-                end
-            }
-        )
+	-- tmux
+	--
+	use({
+		"aserowy/tmux.nvim",
+		config = function()
+			require("tmux").setup()
+		end,
+	})
 
-        -- aux
-        --
-        use("dstein64/vim-startuptime")
-    end
-)
+	-- aux
+	--
+	use("dstein64/vim-startuptime")
+end)
