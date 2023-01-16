10e8945b0a3930255661c688eb54c8f93e3e0ac9
Abner Andino <brain.agam@gmail.com>
2023-01-16 17:45:31 +0000

    update packer

--- lua/bighelmet7/packer.lua
+++ lua/bighelmet7/packer.lua
@@ -61,12 +61,8 @@ return require("packer").startup(function(use)
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.0",
-		requires = { { "nvim-lua/plenary.nvim" } },
+		requires = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
 	})
-
-	-- ripgrep
-	--
-	use("BurntSushi/ripgrep")
 
 	-- lazygit
 	--
