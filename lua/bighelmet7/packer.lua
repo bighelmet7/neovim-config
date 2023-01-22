219a13bc9f2e1655fa8b1e95d164dd972825b3de
Abner Andino <brain.agam@gmail.com>
2023-01-22 19:23:26 +0000

    add barbacue

--- lua/bighelmet7/packer.lua
+++ lua/bighelmet7/packer.lua
@@ -114,6 +114,22 @@ return require("packer").startup(function(use)
 		end,
 	})
 
+	-- barbacue
+	--
+	use({
+		"utilyre/barbecue.nvim",
+		tag = "*",
+		requires = {
+			"neovim/nvim-lspconfig",
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		after = "nvim-web-devicons", -- keep this if you're using NvChad
+		config = function()
+			require("barbecue").setup()
+		end,
+	})
+
 	-- aux
 	--
 	use("dstein64/vim-startuptime")
