fdc202547ec4a0610d6eeb53b413d23056bb4dbe
Abner Andino <brain.agam@gmail.com>
2023-02-14 18:00:20 +0000

    update plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+	-- lsp
+	-- NOTE: nvim-lspconfig doesn't configure keymap by default:
+	-- https://github.com/neovim/nvim-lspconfig#suggested-configuration
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		-- Automatically install LSPs to stdpath for neovim
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+
+		-- Useful status updates for LSP
+		"j-hui/fidget.nvim",
+
+		-- Additional lua configuration, makes nvim stuff amazing
+		"folke/neodev.nvim",
+	},
+}
