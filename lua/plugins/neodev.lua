aa17c2d236955ac6ea4911984232589dbe1aa746
Abner Andino <brain.agam@gmail.com>
2023-02-15 11:04:23 +0000

    move neodev config to the lsp file

--- lua/plugins/neodev.lua
+++ lua/plugins/neodev.lua
@@ -1,12 +1,4 @@
 return {
 	"folke/neodev.nvim",
 	dependencies = { "rcarriga/nvim-dap-ui" },
-	config = function()
-		require("neodev").setup({
-			library = {
-				plugins = { "nvim-dap-ui", types = true },
-			},
-		})
-	end,
-	lazy = true,
 }
