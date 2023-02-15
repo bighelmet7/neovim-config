a782fb9fced5c6fb7e7f09c0fcaa0c3f5625e504
Abner Andino <brain.agam@gmail.com>
2023-02-15 00:44:23 +0000

    refactor telescope

--- lua/bighelmet7/init.lua
+++ lua/bighelmet7/init.lua
@@ -14,6 +14,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
--- Enable telescope fzf native, if installed
-pcall(require("telescope").load_extension, "fzf")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,27 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.0",
+	branch = "0.1.x",
 	dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				},
+			},
+			defaults = {
+				mappings = {
+					i = {
+						["<C-u>"] = false,
+						["<C-d>"] = false,
+					},
+				},
+			},
+		})
+
+		pcall(require("telescope").load_extension, "fzf")
+	end,
 }
