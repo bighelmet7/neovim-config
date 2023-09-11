de014e20e16721a52f42b18037a2e18d67a72339
Abner Andino <brain.agam@gmail.com>
2023-09-11 23:48:23 +0100

    use pcall instead of require

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -1,5 +1,9 @@
-local dbs = require("bighelmet7.dbs")
-vim.g.dbs = dbs
+local has_dbs, dbs = pcall(require, "bighelmet7.dbs")
+if has_dbs then
+    vim.g.dbs = dbs
+else
+    print("no dbs.lua file was found, :DBUI will be using environment values")
+end
 
 vim.api.nvim_create_autocmd("FileType", {
 	desc = "Auto complete for SQL",
