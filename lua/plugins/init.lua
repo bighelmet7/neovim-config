43b8c9463f7bbba197ecbaa1aab8a373409160b6
Abner Andino <brain.agam@gmail.com>
2023-10-03 09:48:20 +0100

    migrate indent blank line to v3

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,10 +7,10 @@ return {
 	--
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
 		config = function()
-			require("indent_blankline").setup({
-				char = "┊",
-				show_trailing_blankline_indent = false,
+			require("ibl").setup({
+				indent = { char = "┊" },
 			})
 		end,
 	},
